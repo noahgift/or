c136d8909c5692167b3428bafd5f877cbd115023
Noah Gift <noah.gift@gmail.com>
2012-06-02 11:28:43 -0700

    adding coin flip game

---
+++ coin_flip.py
@@ -0,0 +1,36 @@
+import random
+import sys
+import math
+
+num_heads = 0
+num_tails = 0
+num_flips = 0
+total_num_flips = 0
+win_bonus_per_game = 8
+total_winning = 0
+iterations = int(sys.argv[1])
+
+for i in range(iterations):
+    while True:
+        value = random.random()
+        num_flips += 1
+        if value <= .499999999999999:
+            num_heads += 1
+        else:
+            num_tails += 1
+        if num_heads - num_tails == math.fabs(3):
+            total_winning += win_bonus_per_game
+            total_num_flips += num_flips
+            print "simulation: %s" % i
+            print "num_heads: %s" % num_heads
+            print "num_tails: %s" % num_tails
+            print "num_flips: %s" % num_flips
+            print "profit for game: %s" % (win_bonus_per_game - num_flips) 
+            num_tails = 0
+            num_heads = 0
+            num_flips = 0
+            break
+        
+net_profit = total_winning - total_num_flips
+print "simulations run: %s" % iterations
+print "net_profit: %s" % net_profit 
